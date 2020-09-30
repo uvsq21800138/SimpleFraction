9ccecd25acb6204f34548ea7acfe6a110fb4cb16
Coralie ZENS <coralie.zens@ens.uvsq.fr>
2020-09-30 14:56:35 +0200

    Modification Fraction.java : Constructeur & toString

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,14 @@
 public class Fraction {
+    private int numerateur;
+    private int denominateur;
+
+    public Fraction(int numerateur, int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    @Override
+    public String toString() {
+        return numerateur + " / " + denominateur;
+    }
 }
