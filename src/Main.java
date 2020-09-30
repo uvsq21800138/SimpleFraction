d106bd7e801a40f811bde60be1eca3ad26790055
Coralie ZENS <coralie.zens@ens.uvsq.fr>
2020-09-30 15:16:54 +0200

    Modification Main.java : Affichage avec test (Assertion) sur classe Fraction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] argv){
-        System.out.println("Bonjour");
+        System.out.println("Fraction :");
+        Fraction f = new Fraction(2,5);
+        System.out.println(f.toString());
+        assert f.toString().equals("2 / 5") : "Erreur d'affichage";
     }
 }
